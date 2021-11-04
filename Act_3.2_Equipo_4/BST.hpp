c30788b0522793c9d24fba57a423492355efd244
aaensmingerg <83736283+aaensmingerg@users.noreply.github.com>
2021-11-03 19:07:42 -0600

    actividad 3.2

---
+++ Act_3.2_Equipo_4/BST.hpp
@@ -0,0 +1,132 @@
+//
+//  BST.hpp
+//  BinaryTree
+//
+//  Created by Vicente Cubells on 20/10/20.
+//
+
+#ifndef BST_hpp
+#define BST_hpp
+
+#include "BinaryTree.hpp"
+
+template <class T>
+class BST : public BinaryTree<T>
+{
+    
+public:
+    BST() {};
+    virtual ~BST() { };
+    
+    TreeNode<T> * search(const T &) const;
+    bool insert(T &);
+    bool insert(TreeNode<T> * );
+
+    void getAltura();
+    void getAncestors(TreeNode<T> * );
+    void getNivel(TreeNode<T> * );
+    
+    
+private:
+    /* Ocultar algunos métodos heredados */
+    //using BinaryTree<T>::insert;
+};
+template <class T>
+TreeNode<T> * BST<T>::search(const T & value) const
+{
+    TreeNode<T> * aux = this->root;
+    
+    while (aux != nullptr) {
+        if (aux->getInfo() == value) {
+            return aux;
+        }
+        else {
+            aux = aux->getInfo() > value ? aux->getLeft() : aux->getRight();
+        }
+    }
+    
+    return nullptr;
+}
+
+template <class T>
+bool BST<T>::insert(T & value)
+{
+    return this->insert(new TreeNode<T>(value));
+}
+
+template <class T>
+bool BST<T>::insert(TreeNode<T> * node )
+{
+    /* No se puede insertar un nodo NULL */
+    if (node == nullptr) return false;
+    
+    /* Obtener la info del nodo a insertar */
+    T value = node->getInfo();
+    
+    /* Establecer un apuntador auxiliar */
+    TreeNode<T> * aux = this->root;
+    
+    /* Estalecer una variable para el padre */
+    TreeNode<T> * parent = nullptr;
+    
+    /* Buscar la posición que le corresponde */
+    while (aux != nullptr) {
+        if (aux->getInfo() == value) {
+            /* Liberar la memoria del nodo porque ya existe */
+            delete node;
+            
+            return false;
+        }
+        else {
+            parent = aux;
+            aux = aux->getInfo() > value ? aux->getLeft() : aux->getRight();
+        }
+    }
+    
+    /* Establecer el parent del nodo a insertar */
+    node->setParent(parent);
+    
+    /* Si el árbol está vacío, se inserta la raiz */
+    if (parent == nullptr) {
+        this->root = node;
+    }
+    else {
+        /* Si el value a insertar es menor, se inserta a la izquierda */
+        if ( parent->getInfo() > value ) {
+            parent->setLeft(node);
+        }
+        /* Si el value a insertar es mayor, se inserta a la derecha */
+        else {
+            parent->setRight(node);
+        }
+    }
+    
+    return true;
+}
+
+template <class T>
+void BST<T>::getAltura(){
+    
+}
+
+template <class T>
+void BST<T>::getAncestors(TreeNode<T> * node){
+    TreeNode<T> * aux = this->root;
+    while (node != aux){
+        std::cout << *node->getParent() << std::endl;
+        node = node->getParent();
+    }
+}
+
+template <class T>
+void BST<T>::getNivel(TreeNode<T> * node){
+    TreeNode<T> * aux = this->root;
+    int cuenta = 0;
+    while (node != aux){
+        node = node->getParent();
+        cuenta++;
+    }
+    std::cout << cuenta << std::endl;
+}
+
+#endif /* BST_hpp */
---
+++ Act_3.2_Equipo_4/File.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include "File.hpp"
+#include "Busqueda.hpp"
+#include "Ordenamiento.hpp"
+#include <algorithm>
+using namespace std;
+        
+
+Fecha File::buscarSegundaFecha(){
+        int encontrados;
+        int inicial;
+        Fecha fechaInicial = fecha[0];
+        Fecha fechaBuscar;
+        for (int i = 0; i < fecha.size(); i++){
+            if (fecha[i] != fechaInicial){
+                inicial = i;
+                fechaBuscar = fecha[i];
+                break;
+            }
+        }
+        while (fecha[inicial] == fechaBuscar){
+            encontrados++;
+            inicial++;
+        }
+        cout << encontrados << endl;
+        return fechaBuscar;
+    }
+
+vector<Fecha> File::getFecha(){
+    return fecha;
+}
+
+vector<string> File::getNombreDestino(){
+    return nombreDestino;
+}
+
+template<class T>
+vector<T> File::valoresUnicos(vector<T> vectorBusqueda){
+    vector<T> unicos = {vectorBusqueda[0]};
+    T valorComparar = vectorBusqueda[0];
+    for(int i=0; i < vectorBusqueda.size(); i++){
+        if(vectorBusqueda[i] != valorComparar){
+            if(i > 0 && vectorBusqueda[i-1] != valorComparar){
+                unicos.push_back(vectorBusqueda[i]);
+                valorComparar = vectorBusqueda[i];
+            }
+        }
+    }
+    return unicos;
+}
+
+void File::busquedaNombre(){
+    vector<string> nombres = {"jeffrey.reto.com","betty.reto.com","katherine.reto.com",
+    "scott.reto.com","benjamin.reto.com","samuel.reto.com","raymond.reto.com","server.reto.com"};
+    for(int i=0; i < nombres.size(); i++){
+        int indice = Busqueda<string>::busquedaSecuencial(nombreOrigen, nombres[i]);
+        if(indice == -1){
+            cout << "El nombre: " << nombres[i] << " |no se encuentra|" << endl;
+        }
+        else{
+            cout << "El nombre: " << nombres[i] << " |si se encuentra| " << endl;
+        }
+
+    }
+}
+
+void File::printNombresUnicos(){
+    vector<string> dummy = nombreOrigen;
+    sort(dummy.begin(), dummy.end(), Ordenamiento<string>::asc);
+    vector<string> unico;
+    unico = valoresUnicos(dummy);
+    for(int i=0; i < unico.size(); i++){
+        cout << "Nombre Registrado: " << unico[i] << endl;
+    }
+}
+vector<string> File::direcccionIterna(){
+    vector<string> almacena;
+    string s1;
+    for(int i=0; i < ipOrigen.size(); i++){
+        if (ipOrigen[i].size() > 1){
+            for(int b=0; b < 10; b++){
+                s1.push_back(ipOrigen[i][b]);
+            }
+            almacena.push_back(s1);
+            s1.clear();
+        }
+        
+    }   
+    
+    return valoresUnicos(almacena);
+}
+
+void File::correosElectronicos(){
+    vector<string> dummy = nombreDestino;
+    sort(dummy.begin(), dummy.end(), Ordenamiento<string>::asc);
+    vector<string> unico;
+    unico = valoresUnicos(dummy);
+    for(int i=0; i < unico.size(); i++){
+        cout << "Direccion registrada: " << unico[i] << endl; 
+    }
+}
+
+void File::puertosUsados(){
+    vector<int> puertos = {};
+    int puerto;
+    for(int i=0; i < puertoDestino.size(); i++){
+        if (puertoDestino[i] != "-"){
+            puerto =  stoi(puertoDestino[i]);  
+            if(puerto<=1000 && find(puertos.begin(), puertos.end(), puerto) == puertos.end()){
+                puertos.push_back(puerto);
+            }
+        }
+    }
+    printPuertos(puertos);
+}
+
+void File::printPuertos(vector<int> puertos){
+    puertos = Ordenamiento<int>::mergesort(puertos,Ordenamiento<int>::asc);
+    cout << "Los puertos encontrados debajo del mil son: " << endl;
+    for (int i=0; i < puertos.size(); i++){
+        if (i == puertos.size()-1){
+            cout << puertos[i] << "." << endl;
+        }
+        else{
+            cout << puertos[i] << ", ";
+        }  
+    }
+}
